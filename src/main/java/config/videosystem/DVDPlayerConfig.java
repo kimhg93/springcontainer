13cabd57af960c2b128a9b56d5c1d8b465b99be6
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-10-15 11:52:04 +0900

    video system xml config

---
+++ src/main/java/config/videosystem/DVDPlayerConfig.java
@@ -0,0 +1,24 @@
+package config.videosystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import kr.co.itcen.springcontainer.videosystem.DVDPlayer;
+import kr.co.itcen.springcontainer.videosystem.DigitalVideoDisc;
+
+@Configuration
+@Import({DVDConfig.class})
+public class DVDPlayerConfig {
+	
+	@Autowired
+	@Qualifier("AvengersInfinityWar")
+	private DigitalVideoDisc dvd;
+	
+	@Bean
+	public DVDPlayer dvdPlayer() {
+		return new DVDPlayer(dvd);
+	}
+}
